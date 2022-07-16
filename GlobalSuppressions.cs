91c05391f4df9b6b35504702151ab9959774ec46
Peter Jannesen <peter@jannesen.com>
2022-07-16 08:29:31 +0200

    Move to buildin NetAnalyzers

--- GlobalSuppressions.cs
+++
@@ -1,4 +0,0 @@
-﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design",        "CA1032:Implement standard exception constructors")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming",        "CA1710:Identifiers should have correct suffix")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style",         "IDE0016:Use 'throw' expression")]
